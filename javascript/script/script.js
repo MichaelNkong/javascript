23fd43175e9306bb28be68994d3be5bad4bf63f6
MichaelNkong <nkongho.ndip@yahoo.com>
2020-08-08 03:46:52 +0200

    tip calculator

--- javascript/script/script.js
+++ javascript/script/script.js
@@ -1,4 +1,35 @@
-var ageJohn=prompt("enter your age john");
-console.log('john');
-var massJohn=prompt('enter your height john');
- var BMI_j=a
+
+
+var tipcalculator={
+bills:[124,48,268,180,42],
+tip:[],
+final:[],
+ 
+cal:function(){
+    ;
+    
+   for(var i=0;i<=this.bills.length; i++){
+
+   if(this.bills[i]<50){
+   this.tip[i]=(this.bills[i])*0.2;
+   this.final[i]=this.bills[i] + this.tip[i];
+   
+   }
+   else if(50 <= this.bills[i] && this.bills[i]<=200){
+    this.tip[i]=(this.bills[i])*0.5;
+    this.final[i]=this.bills[i] + this.tip[i];
+    }
+   else if(this.bills[i]>200){
+    this.tip[i]=(this.bills[i])*0.1;
+    this.final[i]=this.bills[i] + this.tip[i];
+    }
+
+    }
+    
+  }
+
+}
+
+
+tipcalculator.cal();
+console.log(tipcalculator);
